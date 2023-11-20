cf9c682843f9504c0177f1eda95a9bbed4bb21eb
Yasin Baran Kaplan <101335414+YasinBaranKaplan@users.noreply.github.com>
2023-11-21 02:15:49 +0300

    Update question1.java

--- Quiz1/src/question1.java
+++ Quiz1/src/question1.java
@@ -9,12 +9,14 @@ public class question1 {
 		int[] array1 = new int[3];
 		int[] array2 = new int[3];
 		
+		
+		
 		for(int i = 0; i<array1.length; i++) {
 			int index = r.nextInt(dizi.length);
 			array1[i]=dizi[index];
 		}
 		
-		for(int i = 0; i<array1.length; i++) {
+		for(int i = 0; i<array2.length; i++) {
 			int index = r.nextInt(dizi.length);
 			array2[i]=dizi[index];
 		}
@@ -28,9 +30,34 @@ public class question1 {
 			System.out.println(array2[i]);
 		}
 		
-		System.out.println("Yeni dizi.");
+		/*System.out.println("Yeni dizi.");
 		for(int k = 0;k<3;k++) {
 			System.out.println(array1[k]+""+array2[k]+" "+array1[k]*array2[k]);
+		}*/
+		
+		int[][] matris = new int[3][2];
+		
+		for(int i=0;i<matris.length;i++) {
+			for(int j = 0;j<matris[0].length;j++) {
+				if(j==0) {
+					matris[i][j]= array1[i]*10+array2[i];
+				}
+				else {
+					matris[i][j]=array1[i]*array2[i];
+				}
+				
+			}
 		}
+		System.out.println();
+		System.out.println("Yeni dizi =>");
+		
+		for(int i=0;i<matris.length;i++) {
+			for(int j = 0;j<matris[0].length;j++) {
+				System.out.print(matris[i][j]+" ");
+				
+			}
+			System.out.println();}
+		
+			
+	   }
 	}
-}
